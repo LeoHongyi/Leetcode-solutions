f195696baaaa773cdc7ba60ca83bcbbefd47bed8
Qihao He <qi.he@maine.edu>
2019-08-13 21:24:11 -0700

    import from leetcode

---
+++ src/Jewels and Stones/unit test file.py
@@ -0,0 +1,9 @@
+import sys
+# print "This is the name of the script: ", sys.argv[0]
+# print "Number of arguments: ", len(sys.argv)
+# print "The arguments are: " , str(sys.argv)
+J = sys.argv[1]
+S = sys.argv[2]
+
+print "n: ", n
+        
