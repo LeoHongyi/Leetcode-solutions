af1c391f1f254d2eaaea3fb49a6dde3d4f372c24
Qihao He <qi.he@maine.edu>
2019-08-27 10:26:30 -0700

    Nested Queries

--- hello_world.sql
+++ hello_world.sql
@@ -210,3 +210,24 @@ HAVING count(r.rental_id) >= 3
   ORDER BY
     3 desc
   ;
+
+  -- Rentals per customer
+SELECT
+    rpc.num_rentals,
+    count(distinct rpc.customer_id) as num_customers,
+    sum(p.amount) as total_revenue
+FROM
+    (SELECT
+        r.customer_id, 
+        count(distinct r.rental_id) as num_rentals
+    FROM
+        rental r
+    GROUP BY
+        1
+    ) as rpc,
+    payment p
+WHERE
+    rpc.customer_id = p.customer_id
+    AND rpc.num_rentals > 20
+GROUP BY
+    1;
