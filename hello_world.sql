3cc16259b79f6cbadb2a12842f792c049ef81785
Qihao He <qi.he@maine.edu>
2019-08-27 10:41:11 -0700

    Temporary tables

--- hello_world.sql
+++ hello_world.sql
@@ -232,3 +232,22 @@ WHERE
   AND rpc.num_rentals > 20
 GROUP BY
   1;
+
+-- Rentals per customer
+create temporary table rpc as
+SELECT
+  r.customer_id,
+  count(distinct r.rental_id) as num_rentals
+FROM
+  rental r
+GROUP BY
+  1;
+  
+SELECT
+  sum(p.amount) as total_revenue
+FROM
+  rpc,
+  payment p
+WHERE
+  rpc.customer_id = p.customer_id
+  AND rpc.num_rentals > 20;
