026f816fc4754cabcf0f88a1f48c81a0581ffb7c
Qihao He <qi.he@maine.edu>
2019-08-27 10:29:20 -0700

    format

--- hello_world.sql
+++ hello_world.sql
@@ -211,23 +211,24 @@ HAVING count(r.rental_id) >= 3
     3 desc
   ;
 
-  -- Rentals per customer
+-- Rentals per customer
 SELECT
-    rpc.num_rentals,
-    count(distinct rpc.customer_id) as num_customers,
-    sum(p.amount) as total_revenue
+  rpc.num_rentals,
+  count(distinct rpc.customer_id) as num_customers,
+  sum(p.amount) as total_revenue
 FROM
-    (SELECT
-        r.customer_id, 
-        count(distinct r.rental_id) as num_rentals
+  (
+    SELECT
+      r.customer_id,
+      count(distinct r.rental_id) as num_rentals
     FROM
-        rental r
+      rental r
     GROUP BY
-        1
-    ) as rpc,
-    payment p
+      1
+  ) as rpc,
+  payment p
 WHERE
-    rpc.customer_id = p.customer_id
-    AND rpc.num_rentals > 20
+  rpc.customer_id = p.customer_id
+  AND rpc.num_rentals > 20
 GROUP BY
-    1;
+  1;
