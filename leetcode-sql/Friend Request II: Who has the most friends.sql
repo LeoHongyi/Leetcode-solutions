b6198bcdecdf25e842462fd98e488973696d30ee
Qihao He <qi.he@maine.edu>
2019-09-10 11:17:36 -0700

    union id

---
+++ leetcode-sql/Friend Request II: Who has the most friends.sql
@@ -0,0 +1,18 @@
+SELECT 
+    id1 AS id,
+    COUNT(*) AS num
+FROM 
+    (SELECT 
+        requester_id AS id1, 
+        accepter_id AS id2 
+    FROM 
+        request_accepted
+    UNION
+    SELECT 
+        accepter_id AS id1 , 
+        requester_id AS id2 
+    FROM 
+        request_accepted) a
+GROUP BY 1
+ORDER BY COUNT(*) DESC
+LIMIT 1;
