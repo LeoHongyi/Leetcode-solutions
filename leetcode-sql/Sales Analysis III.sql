e841918c1cc0e4c7f4cb352b1be04b7f49d983f0
Qihao He <qi.he@maine.edu>
2019-09-10 22:01:58 -0700

    not between to exclude

--- leetcode-sql/Sales Analysis III.sql
+++ leetcode-sql/Sales Analysis III.sql
@@ -1,10 +1,10 @@
-SELECT
-    s.product_id,
-    p.product_name
-FROM
-    Sales s
-        JOIN
-    Product p on s.product_id = p.product_id
-WHERE
-    s.sale_date > '2019-01-01'
-    AND s.sale_date < '2019-03-31';
+select 
+    product_id, product_name
+from 
+    product
+where product_id not in(
+    select 
+        product_id
+    from 
+        sales
+    where sale_date not between '2019-01-01' and '2019-03-31');
