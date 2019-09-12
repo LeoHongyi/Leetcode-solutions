0b6a2a8fff7214fc8ee3167eab4d35eeb0137fe0
Qihao He <qi.he@maine.edu>
2019-09-12 15:48:53 -0700

    right join

--- leetcode-sql/Sales Person.sql
+++ leetcode-sql/Sales Person.sql
@@ -1,11 +1,9 @@
-SELECT
-    name
-FROM
-    salesperson
-WHERE sales_id NOT IN (
-    SELECT
-        sales_id
-    FROM
-        orders
-    WHERE
-        com_id = 1);
+select 
+    salesperson.name
+from 
+    orders o 
+        join 
+    company c on (o.com_id = c.com_id and c.name = 'RED')
+        right join 
+    salesperson on salesperson.sales_id = o.sales_id
+where o.sales_id is null
