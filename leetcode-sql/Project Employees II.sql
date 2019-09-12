8a63014936248ab8eaebdd82b9b03d70ab506a12
Qihao He <qi.he@maine.edu>
2019-09-12 10:56:44 -0700

    count group by

--- leetcode-sql/Project Employees II.sql
+++ leetcode-sql/Project Employees II.sql
@@ -1,7 +1,12 @@
 SELECT
     project_id
 FROM
-    Project p
-GROUP BY project_id
-ORDER BY COUNT(*) DESC
-LIMIT 1;
+    Project
+HAVING project_id = (
+    SELECT
+        COUNT(*)
+    FROM
+        Project p
+    GROUP BY project_id
+    ORDER BY COUNT(*) DESC
+    LIMIT 1);
