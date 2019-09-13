a6a6b325c8ecf0196df5b18010c8a629a250e713
Qihao He <qi.he@maine.edu>
2019-09-12 23:06:52 -0700

    k closest to target

---
+++ src/Binary Search/KClosestInSortedArray.java
@@ -0,0 +1,30 @@
+public class Solution {
+  public int[] kClosest(int[] array, int target, int k) {
+    // Write your solution here
+    if (array.length == 0) {
+      return new int[0];
+    }
+    int left = 0;
+    int right = array.length - 1;
+    while (left < right - 1) {
+      int mid = left + (right - left) / 2;
+      if (array[mid] <= target) {
+        left = mid;
+      } else {
+        right = mid;
+      }
+    }
+    int[] results = new int[k];
+    for (int i = 0; i < k; i++) {
+      if (right >= array.length || left >= 0 && 
+      Math.abs(array[left] - target) <  Math.abs(array[right] - target)) {
+        results[i] = array[left];
+        left--;
+      } else {
+        results[i] = array[right];
+        right++;
+      }
+    }
+    return results;
+  }
+}
