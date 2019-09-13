16a3a23db4baa674538c5bf8d13e2a66a32bdd9e
Qihao He <qi.he@maine.edu>
2019-09-12 17:47:51 -0700

    null, empty

---
+++ src/Debug/ComputeSum.java
@@ -0,0 +1,12 @@
+public class ComputeSum {
+  public int sum(int[] array) {
+    if (array == null || array.length == 0) {
+      return -1;
+    }
+    int s = 0;
+    for (int i = 0; i < array.length; i++) {
+      s += array[i];
+    }
+    return s;
+  }
+}
