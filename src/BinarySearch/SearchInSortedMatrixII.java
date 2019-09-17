3b3560e95f5002aa14db4805ade7016a3c451bda
Qihao He <qi.he@maine.edu>
2019-09-16 23:42:11 -0700

    Search in Sorted Matrix II

--- src/BinarySearch/SearchInSortedMatrixII.java
+++ src/BinarySearch/SearchInSortedMatrixII.java
@@ -1,23 +1,24 @@
 package BinarySearch;
 
 public class SearchInSortedMatrixII {
-
     public int[] search(int[][] matrix, int target) {
         // Write your solution here
         if (matrix.length == 0 || matrix[0].length == 0) {
             return new int[] {-1, -1};
         }
-        int row = matrix.length, col = matrix[0].length;
-        int i = 0, j = col - 1;
-        while (i < row && j >= 0) {
-            if (matrix[i][j] == target) {
-                return new int[] {i, j};
-            } else if (matrix[i][j] > target) {
-                j--;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int rowsIndex = 0;
+        int colsIndex = cols - 1;
+        while (rowsIndex < rows && colsIndex >= 0) {
+            if (matrix[rowsIndex][colsIndex] == target) {
+                return new int[] {rowsIndex, colsIndex};
+            } else if (matrix[rowsIndex][colsIndex] > target) {
+                colsIndex--;
             } else {
-                i++;
+                rowsIndex++;
             }
         }
-        return new int[]{-1, -1};
+        return new int[] {-1, -1};
     }
 }
