1310f33a18b1450c7964aafcff96f2f72f0ecee2
Qihao He <qi.he@maine.edu>
2019-09-17 10:48:01 -0700

    Square root I

---
+++ src/BinarySearch/SquareRootI.java
@@ -0,0 +1,21 @@
+package BinarySearch;
+
+public class SquareRootI {
+    public int sqrt(int x) {
+        // Write your solution here
+        if (x == 0) {
+            return x;
+        }
+        int left = 1, right = x, result = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                left = mid + 1;
+                result = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+}
