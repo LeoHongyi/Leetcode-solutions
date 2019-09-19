6e6657943e609fce982a8738851ac3f636a5d1e9
Qihao He <qi.he@maine.edu>
2019-09-18 21:01:17 -0700

    Missing Number II

--- src/BinarySearch/MissingNumberII.java
+++ src/BinarySearch/MissingNumberII.java
@@ -11,4 +11,24 @@ public class MissingNumberII {
         }
         return i;
     }
+
+    public int binarySearchMissing(int[] array) {
+        // Write your solution here
+        if (array.length == 0) {
+            return 1;
+        }
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] == mid + 1 && array[mid + 1] == mid + 3)  {
+                return mid + 2;
+            } else if (array[mid] == mid + 2 && array[mid + 1] == mid + 3) {
+                right = mid;
+            } else if (array[mid] == mid + 1 && array[mid + 1] == mid + 2){
+                left = mid + 1;
+            }
+        }
+        return right + 1;
+    }
 }
---
+++ test/BinarySearch/MissingNumberIITest.java
@@ -0,0 +1,50 @@
+package BinarySearch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MissingNumberIITest {
+    @Test
+    public void test1() {
+        int[] input = new int[] {1,2,3,4,5,6,8};
+        MissingNumberII mn = new MissingNumberII();
+        int result = mn.missing(input);
+        assertEquals(7, result);
+    }
+    @Test
+    public void test2() {
+        int[] input = new int[] {};
+        MissingNumberII mn = new MissingNumberII();
+        int result = mn.missing(input);
+        assertEquals(1, result);
+    }
+    @Test
+    public void test3() {
+        int[] input = new int[] {1,3};
+        MissingNumberII mn = new MissingNumberII();
+        int result = mn.missing(input);
+        assertEquals(2, result);
+    }
+    @Test
+    public void test4() {
+        int[] input = new int[] {1,2,3,4,5,6,8};
+        MissingNumberII mn = new MissingNumberII();
+        int result = mn.binarySearchMissing(input);
+        assertEquals(7, result);
+    }
+    @Test
+    public void test5() {
+        int[] input = new int[] {};
+        MissingNumberII mn = new MissingNumberII();
+        int result = mn.binarySearchMissing(input);
+        assertEquals(1, result);
+    }
+    @Test
+    public void test6() {
+        int[] input = new int[] {1,3};
+        MissingNumberII mn = new MissingNumberII();
+        int result = mn.binarySearchMissing(input);
+        assertEquals(2, result);
+    }
+}
