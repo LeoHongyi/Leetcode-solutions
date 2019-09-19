94333b4a721305abbcf87034119b53c65067cc3b
Qihao He <qi.he@maine.edu>
2019-09-19 11:37:42 -0700

    binary search links

--- src/Recursion/AToThePowerOfB.java
+++ src/Recursion/AToThePowerOfB.java
@@ -1,19 +1,19 @@
 package Recursion;
-
 //https://app.laicode.io/app/problem/13
 public class AToThePowerOfB {
     public long power(int a, int b) {
-        // Write your solution here
-        if (b == 0 || a == 1) {
+        if (b == 0) {
             return 1;
         } else if (a == 0) {
             return 0;
+        } else if (a == 1) {
+            return 1;
         }
         long half = power(a, b / 2);
-        if (b % 2 == 0) {
-            return half * half;
+        if (b % 2 != 0) {
+            return a * half * half;
         } else {
-            return half * half * a;
+            return half * half;
         }
     }
 }
