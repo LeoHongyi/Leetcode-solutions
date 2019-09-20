e591b94d0f4bf4f9d0411f4098b93579145026b7
Qihao He <qi.he@maine.edu>
2019-09-20 14:33:27 -0700

    sort with two stacks using selection sort

--- src/sorting/SortWithTwoStacks.java
+++ src/sorting/SortWithTwoStacks.java
@@ -1,17 +1,17 @@
 package sorting;
 import java.util.LinkedList;
 /**
- Method: use s2 as a buffer and a storage for global MIN,
- s1: used for storing all unsorted elements
- s2: left part | right part
- left part: to store all sorted elements in previous iterations
- right part: used as a buffer to find the current smallest element
- 代码在具体的流程应该是
- 1. 把s1中的所有m个元素倒到s2中，在这个过程中找到s1中的最小值
- 2. 把s2中最上面的m个元素除了最小值之外再倒回s1
- 3. 把最小值放到s2中
- 4. 重复循环上面的1，2，3直到s1中元素为0
- **/
+ * Method: use s2 as a buffer and a storage for global MIN,
+ *  s1: used for storing all unsorted elements
+ *  s2: left part | right part
+ *  left part: to store all sorted elements in previous iterations
+ *  right part: used as a buffer to find the current smallest element
+ *  代码在具体的流程应该是
+ *  1. 把s1中的所有m个元素倒到s2中，在这个过程中找到s1中的最小值
+ *  2. 把s2中最上面的m个元素除了最小值之外再倒回s1
+ *  3. 把最小值放到s2中
+ *  4. 重复循环上面的1，2，3直到s1中元素为0
+ */
 public class SortWithTwoStacks {
 	public void sort(LinkedList<Integer> s1) {
 		LinkedList<Integer> s2 = new LinkedList<Integer>();
