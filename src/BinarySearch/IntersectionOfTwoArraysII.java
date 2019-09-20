854de4a464ff4bb64687c210df95233787b0db8a
Qihao He <qi.he@maine.edu>
2019-09-19 21:40:49 -0700

    Intersection of two arrays II format

--- src/BinarySearch/IntersectionOfTwoArraysII.java
+++ src/BinarySearch/IntersectionOfTwoArraysII.java
@@ -14,11 +14,14 @@ public class IntersectionOfTwoArraysII {
         Arrays.sort(nums2);
         int i=0,j=0,k=0;
         while(i<nums1.length && j<nums2.length){
-            if(nums1[i]<nums2[j] )
+            if (nums1[i]<nums2[j] ) {
                 i++;
-            else if(nums2[j]<nums1[i])
+            }
+
+            else if (nums2[j]<nums1[i]) {
                 j++;
-            else if(nums1[i]==nums2[j]){
+            }
+            else if (nums1[i]==nums2[j]) {
                 nums1[k++]=nums1[i];
                 i++;
                 j++;
