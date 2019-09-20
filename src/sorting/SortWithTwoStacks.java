ee60281b0c414e10353a2b2f0aaa8a638d91b043
Qihao He <qi.he@maine.edu>
2019-09-20 15:33:57 -0700

    remove dup code

--- src/sorting/SortWithTwoStacks.java
+++ src/sorting/SortWithTwoStacks.java
@@ -13,44 +13,6 @@ import java.util.LinkedList;
  *  4. 重复循环上面的1，2，3直到s1中元素为0
  */
 public class SortWithTwoStacks {
-	public void sort(LinkedList<Integer> s1) {
-		LinkedList<Integer> s2 = new LinkedList<Integer>();
-		// Write your solution here.
-		if (s1.isEmpty()) {
-			return;
-		}
-		// count the numbers that are sorted.
-		int count = 0;
-		int n = s1.size();
-		while (count < n) {
-			// reset min to MAX_VALUE
-			int min = Integer.MAX_VALUE;
-			// step 1: get s1 all elements into s2 and find min.
-			while (s1.size() > 0) {
-				min = Math.min(min, s1.peekFirst());
-				s2.offerFirst(s1.pollFirst());
-			}
-			// step 2: get s2 to s1 (except the min)
-			int duplicate = 0;
-			while (s2.size() > count) {
-				int temp = s2.pollFirst();
-				if (temp != min) {
-					s1.offerFirst(temp);
-				} else {
-					duplicate++;
-				}
-			}
-			// step 3: offer min value into s2
-			for (int i = 0; i < duplicate; i++) {
-				s2.offerFirst(min);
-				count++;
-			}
-		}
-		while (!s2.isEmpty()) {
-			s1.offerFirst(s2.pollFirst());
-		}
-	}
-
 	public void selectionSortWithTwoStacks(LinkedList<Integer> s1) {
 		LinkedList<Integer> s2 = new LinkedList<Integer>();
 		// Write your solution here.
