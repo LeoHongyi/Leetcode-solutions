60423a2bd32df0c1c6c5b9e668b95a47ae152178
Qihao He <qi.he@maine.edu>
2019-09-21 23:58:47 -0700

    rainbow sort i with 3 pointers

--- src/sorting/RainbowSort.java
+++ src/sorting/RainbowSort.java
@@ -1,7 +1,36 @@
 package sorting;
 
 public class RainbowSort {
-	public static int[] rainbowSort(int[] array) {
+	public int[] rainbowSort(int[] array) {
+		// Write your solution here
+		if (array == null || array.length <= 1) {
+			return array;
+		}
+		int i = 0;
+		int j = 0;
+		int k = array.length - 1;
+		while (j <= k) {
+			if (array[j] == -1) {
+				swap(array, i, j);
+				i++;
+				j++;
+			} else if (array[j] == 1) {
+				swap(array, j, k);
+				k--;
+			} else {
+				j++;
+			}
+		}
+		return array;
+	}
+	private void swap(int[] array, int left, int right) {
+		int temp = array[left];
+		array[left] = array[right];
+		array[right] = temp;
+		return;
+	}
+
+	public int[] rainbowSort2(int[] array) {
 		// Write your solution here
 		if (array.length <= 1) {
 			return array;
