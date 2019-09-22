aab5fa57c37a590d81c58b4064662366d3eb3bb4
Qihao He <qi.he@maine.edu>
2019-09-22 15:27:15 -0700

    insertion sort time space complexity

--- src/Algorithms/Sort/InsertionSort.java
+++ src/Algorithms/Sort/InsertionSort.java
@@ -1,6 +1,13 @@
 package Algorithms.Sort;
 
 public class InsertionSort {
+    /**
+     * @author Qihao He
+     * @date 09/22/2019
+     * https://app.laicode.io/app/problem/340
+     * Method: insertion sort
+     * time: O(n^2), space: O(1)
+     */
     public int[] sort(int[] array) {
         for (int i = 1; i < array.length; i++) {
             int curValue = array[i];
