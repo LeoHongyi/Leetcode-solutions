aac5832c4a6bf26a5d9fdc8b963adeaccb5ea593
Qihao He <qi.he@maine.edu>
2019-09-22 18:34:04 -0700

    Java streams example

--- src/Algorithms/Stream/JavaStreams.java
+++ src/Algorithms/Stream/JavaStreams.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.stream.IntStream;
 
 public class JavaStreams {
-    public static void main() throws IOException {
+    public static void main(String[] args) throws IOException {
 
         // 1. Integer Stream
         IntStream
