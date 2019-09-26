53cb54e5cc0d59e9cc6ccb814deb048d0f481e9b
Qihao He <qi.he@maine.edu>
2019-09-25 23:32:34 -0700

    longest palindromeic substring

--- src/algorithms/dynamicProgramming/LongestPalindromicSubstring.java
+++ src/algorithms/dynamicProgramming/LongestPalindromicSubstring.java
@@ -49,6 +49,26 @@ public class LongestPalindromicSubstring {
         return s.substring(index, index + globalMax);
     }
 
+    public String longestPalindromeDP2(String s) {
+        int n = s.length();
+        String res = null;
+
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) &&
+                        (j - i < 3 || dp[i + 1][j - 1]);
+
+                if (dp[i][j] &&
+                        (res == null || j - i + 1 > res.length())) {
+                    res = s.substring(i, j + 1);
+                }
+            }
+        }
+        return res;
+    }
+
     /**
      * Solution 2: traverse and expand
      * Traverse each possible index as the middle of a palindrom and expand
