f35dd7aed14afb7e7db6c97f1ced19fd96a8a889
Qihao He <qi.he@maine.edu>
2019-09-27 11:35:07 -0700

    movies on flight, amazon oa

---
+++ src/algorithms/interviewedTasks/MoviesOnFlight.java
@@ -0,0 +1,22 @@
+package algorithms.interviewedTasks;
+/**
+ * I had 2 question on my online assesment, however I remeber only the first one.
+ * My code passed only 10 test out of 13. I did a sorting and then found the best pair with 2 for loops
+ *
+ * Question:
+ * You are on a flight and wanna watch two movies during this flight.
+ * You are given List<Integer> movieDurations which includes all the movie durations.
+ * You are also given the duration of the flight which is d in minutes.
+ * Now, you need to pick two movies and the total duration of the two movies is less than or equal to (d - 30min).
+ *
+ * Find the pair of movies with the longest total duration and return they indexes. If multiple found,
+ * return the pair with the longest movie.
+ *
+ * Example 1:
+ *
+ * Input: movieDurations = [90, 85, 75, 60, 120, 150, 125], d = 250
+ * Output: [0, 6]
+ * Explanation: movieDurations[0] + movieDurations[6] = 90 + 125 = 215 is the maximum number within 220 (250min - 30min)
+ */
+public class MoviesOnFlight {
+}
