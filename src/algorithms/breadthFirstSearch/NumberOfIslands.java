c4ead038ddb291af3af337db1fea69968ba7e392
Qihao He <qi.he@maine.edu>
2019-09-26 22:34:56 -0700

    number of islands

--- src/algorithms/breadthFirstSearch/NumberOfIslands.java
+++ src/algorithms/breadthFirstSearch/NumberOfIslands.java
@@ -57,37 +57,37 @@ public class NumberOfIslands {
     /**
      * Method: DFS
      */
-    private int[] dirX = {0, 0, -1, 1};
-    private int[] dirY = {1, -1, 0, 0};
-
-    public int numIslandsDFS(char[][] grid) {
-        if (grid.length == 0 || grid[0].length == 0) {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
             return 0;
         }
-        int counter = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    counter++;
-                    dfs(grid, i, j);
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
                 }
             }
         }
-        return counter;
+        return num_islands;
     }
 
-    private boolean inBound(int i, int j, int row, int col) {
-        return i >= 0 && i < row && j >= 0 && j < col;
-    }
+    private void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
 
-    private void dfs(char[][] grid, int i, int j) {
-        if (!inBound(i, j, grid.length, grid[0].length) || grid[i][j] == '0') {
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
             return;
         }
-        grid[i][j] = '0';
-        for (int k = 0; k < 4; k++) {
-            dfs(grid, i + dirX[k], j + dirY[k]);
-        }
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
     }
 }
 
