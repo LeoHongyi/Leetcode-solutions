b053e27a163feeb1f1723150d62d4e53beef36f2
Qihao He <qi.he@maine.edu>
2019-09-27 15:55:48 -0700

    refactor

--- src/algorithms/dynamicProgramming/LargestSubArrarySum.java
+++ src/algorithms/dynamicProgramming/LargestSubArrarySum.java
@@ -1,18 +1,19 @@
 package algorithms.dynamicProgramming;
-/**
- * @author Qihao He
- * @date 09/24/2019
- * https://app.laicode.io/app/problem/489
- * Method: DP from left to right linear scan.
- *  curMax[i] represent the max subArray sum inluding the array[i]
- *  curMax[0] = array[0]
- *  curMax[i] = max(array[i], curMax[i - 1] + array[i])
- *  curMax[i] = array[i]    if curMax[i - 1] < 0
- *  =  curMax[i - 1] + array[i]    if curMax[i - 1] >= 0
- *  update gloMax when curMax > gloMax
- *  time O(n), space O(1)
- */
+
 public class LargestSubArrarySum {
+    /**
+     * @author Qihao He
+     * @date 09/24/2019
+     * https://app.laicode.io/app/problem/489
+     * Method: DP from left to right linear scan.
+     *  curMax[i] represent the max subArray sum inluding the array[i]
+     *  curMax[0] = array[0]
+     *  curMax[i] = max(array[i], curMax[i - 1] + array[i])
+     *  curMax[i] = array[i]    if curMax[i - 1] < 0
+     *  =  curMax[i - 1] + array[i]    if curMax[i - 1] >= 0
+     *  update gloMax when curMax > gloMax
+     *  time O(n), space O(1)
+     */
     public int[] largestSum(int[] array) {
         // Write your solution here
         if (array.length == 1) {
@@ -40,4 +41,32 @@ public class LargestSubArrarySum {
         }
         return new int[]{gloMax, gloStart, gloEnd};
     }
+
+    /**
+     * @author Qihao He
+     * @date 09/24/2019
+     * https://app.laicode.io/app/problem/97
+     * Assumption: array != null && length >= 1
+     *  Method: DP, from left to right
+     *  dp[i] the largest sum ending at index i
+     *  dp[i] = array[i]  if dp[i - 1] <= 0
+     *  = dp[i - 1] + array[i]  else
+     *  time: O(n), space: O(1)
+     */
+    public class LargestSubArraySum {
+        public int largestSum(int[] array) {
+            // Write your solution here
+            // initialize as 1st element
+            int curMax = array[0];
+            int result = array[0];
+            for (int i = 1; i < array.length; i++) {
+                // case 1, 2
+                curMax = Math.max(array[i], curMax + array[i]);
+                // update global max
+                result = Math.max(result, curMax);
+            }
+            return result;
+        }
+    }
+
 }
