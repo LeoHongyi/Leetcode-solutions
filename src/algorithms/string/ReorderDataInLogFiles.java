29600edc65eebca72e0dfd1db37505994d5e50b0
Qihao He <qi.he@maine.edu>
2019-09-28 22:50:52 -0400

    reorder data in log files

--- src/algorithms/string/ReorderDataInLogFiles.java
+++ src/algorithms/string/ReorderDataInLogFiles.java
@@ -13,25 +13,26 @@ public class ReorderDataInLogFiles {
         Arrays.sort(logs, new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
-                int s1si = s1.indexOf(' ');
-                int s2si = s2.indexOf(' ');
-                char s1fc = s1.charAt(s1si+1);
-                char s2fc = s2.charAt(s2si+1);
+                int s1SpaceIndex = s1.indexOf(' ');
+                int s2SpaceIndex = s2.indexOf(' ');
+                char s1FollowingChar = s1.charAt(s1SpaceIndex+1);
+                char s2FollowingChar = s2.charAt(s2SpaceIndex+1);
 
-                if (s1fc <= '9') {
-                    if (s2fc <= '9') {
+                if (s1FollowingChar <= '9') {
+                    // char <= 9 is digit log
+                    if (s2FollowingChar <= '9') {
                         return 0;
                     } else {
                         return 1;
                     }
                 }
-                if (s2fc <= '9') {
+                if (s2FollowingChar <= '9') {
                     return -1;
                 }
 
-                int preCompute = s1.substring(s1si+1).compareTo(s2.substring(s2si+1));
+                int preCompute = s1.substring(s1SpaceIndex+1).compareTo(s2.substring(s2SpaceIndex+1));
                 if (preCompute == 0) {
-                    return s1.substring(0,s1si).compareTo(s2.substring(0,s2si));
+                    return s1.substring(0,s1SpaceIndex).compareTo(s2.substring(0,s2SpaceIndex));
                 }
                 return preCompute;
             }
