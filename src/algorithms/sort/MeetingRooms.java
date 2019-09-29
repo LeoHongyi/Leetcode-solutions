182068df5c1fa1a425ac1834ceae661d577c35e8
Qihao He <qi.he@maine.edu>
2019-09-28 23:52:46 -0400

    meeting room I sort

--- src/algorithms/sort/MeetingRooms.java
+++ src/algorithms/sort/MeetingRooms.java
@@ -2,13 +2,19 @@ package algorithms.sort;
 
 import java.util.Arrays;
 import java.util.Comparator;
-
+/**
+ * @author Qihao He
+ * @date 09/28/2019
+ * https://leetcode.com/problems/meeting-rooms/
+ * https://app.laicode.io/app/problem/482
+ */
 public class MeetingRooms {
     public boolean canAttendMeetings(int[][] intervals) {
         if(intervals == null) {
             return false;
         }
         Arrays.sort(intervals, new Comparator<int[]>(){
+            @Override
             public int compare(int[] i1, int[] i2){
                 return i1[0]-i2[0];
             }
